fad8fc90c1222ff9270bfbce3c746deae9534fe6
Greg Tozzi <gregtozzi@Greg-Tozzi-iMac.local>
2017-02-13 15:14:14 -0500

    Major edits

--- explore_data.R
+++ explore_data.R
@@ -1,18 +1,34 @@
 require(igraph)
 require(magrittr)
+source('helper_functions.R')
 
-Attiro <- read_graph('Attiro.net',
-                     format = 'pajek')
+Attiro <- read_graph('Attiro.graphml',
+                     format = 'graphml')
+
+Attiro <- as.directed(Attiro)
+
 Attiro_Grouping <- read.csv('Attiro_Grouping.csv')
 V(Attiro)$label <- Attiro_Grouping$Label %>% as.character
-V(Attiro)$group <- Attiro_Grouping$group
+Group <- Attiro_Grouping$group
+V(Attiro)$group <- Group
+Palette <- character()
+for(i in 1:length(unique(Group))) {
+  Palette[which(Group == unique(Group)[i])] <- colors_ok_go[i]
+}
+V(Attiro)$color <- unlist(Palette)
+E(Attiro)$type <- E(Attiro)$weight
+delete_edge_attr(Attiro, 'weight')
 
 plot(Attiro,
-     vertex.color = V(Attiro)$group,
      vertex.label = V(Attiro)$label,
-     edge.color = E(Attiro)$weight)
+     edge.color = E(Attiro)$type,
+     edge.arrow.size = 0.3,
+     vertex.label.family = "Futura Medium",
+     vertex.label.color = 'white',
+     vertex.frame.color = 'white')
 
-GN <- cluster_edge_betweenness(Attiro,
-                               weights = NULL)
+GN <- cluster_edge_betweenness(Attiro, directed = FALSE)
 
-write_graph(Attiro, 'newAttiro', format = 'pajek')
+Attiro_undirected <- as.undirected(Attiro, mode = 'collapse')
+FG <- cluster_fast_greedy(Attiro_undirected)
+FG_mem <- membership(FG)
---
+++ helper_functions.R
@@ -0,0 +1,27 @@
+colors_ok_go <- list(rgb(2/255, 66/255, 249/255),
+                     rgb(232/255, 46/255, 20/255),
+                     rgb(61/255, 182/255, 98/255),
+                     rgb(252/255, 209/255, 10/255),
+                     rgb(0/255, 214/255, 251/255),
+                     rgb(247/255, 71/255, 13/255),
+                     rgb(119/255, 22/255, 255/255),
+                     "#B7CB96", "#AF9979", "#827711", 'black',
+                     "#F29525", "#B9177D")
+
+colors_edges <- c(rgb(119/255, 22/255, 255/255),
+                     "#FF2851", "#8E8E93")
+
+assign_colors <- function(membership, Palette) {
+  sapply(membership, function(x) Palette[[x]])
+}
+
+plot_Attiro <- function(graph, vertexColors) {
+  plot(graph,
+       vertex.label = V(graph)$label,
+       vertex.color = vertexColors,
+       edge.arrow.size = 0.3,
+       vertex.label.family = "Futura Medium",
+       vertex.label.color = 'white',
+       vertex.frame.color = 'white',
+       vertex.label.cex = 0.5)
+}
